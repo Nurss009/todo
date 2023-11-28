195bc6b54e3749445eb3164dd32c01978465c19e
Nursultan <tupoukidala77@gmail.com>
2023-11-28 18:57:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import dayjs from "dayjs";
+import EditModal from "./Components/EditModals/EditModal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const url ="https://656424f6ceac41c0761d8037.mockapi.io/Shema"
+
+const App = ()=>{
+    const [todos, setTodos]=useState([])
+    const [todoAdd, setTodoAdd]=useState('')
+    const [editModalOpen, setEditModalOpen] =useState(false)
+    const [idTodo, setIdTodo] = useState('')
+
+
+    useEffect(() => {
+        axios('https://656424f6ceac41c0761d8037.mockapi.io/Shema')
+            .then(({data})=>setTodos(data))
+    }, []);
+
+
+    const handleEdit = (idTodo) => {
+        setEditModalOpen(true)
+        setIdTodo(idTodo)
+    }
+
+    const handleAddTodo=()=>{
+        const newTodo ={
+            title: todoAdd,
+            completed: false,
+            createdit: null,
+            completedAt: +new Date()
+
+        }
+        setTodoAdd('')
+        axios.post(url,newTodo)
+            .then(({data})=>setTodos([...todos,data]))
+    }
+
+    const handleDeleteTodo = (todo)=>{
+        axios.delete(`${url}/${todo.id}`, todo)
+            .then(({data})=> setTodos(todos.filter((todo)=> todo.id!==data.id)))
+    }
+
+    const handleSave = (todo) => {
+        axios.put(`${url}/${idTodo}`, todo)
+            .then(({data}) => {
+                setTodos(todos.map(el => el.id === data.id ? data: el))
+            })
+    }
+
+    return(
+        <div className={'container'}>
+            <h1>Todo List</h1>
+            <input className={'Input'}  placeholder={"Добавить"} type="text" onChange={(e)=> setTodoAdd(e.target.value)}/>
+            <button className={'Butt'} onClick={handleAddTodo}>Add</button>
+            <div>
+
+                {
+                    editModalOpen &&
+                    <EditModal handleSave={handleSave} setOpen={setEditModalOpen} idTodo={idTodo} url={url}/>
+                }
+                {
+                    todos.map(todo=>
+                        <div className={'todo-wrapper'} key={todo.id}>
+                            <p>{todo.title}</p>
+                            <input type="checkbox" checked={todo.completed} />
+                            <span>
+                                {dayjs(todo.createdAt).format('HH:mm DD:MM:YYYY')}
+                            </span>
+                            <span>
+                                {dayjs(todo.createdAt).format('HH:mm DD:MM:YYYY')}
+                            </span>
+                            <button className={'buta'} onClick={()=>handleDeleteTodo(todo)}>Delete</button>
+                            <button onClick={() => handleEdit(todo.id)}>Edit</button>
+                        </div>)
+                }
+            </div>
+        </div>
+    )
 }
-
-export default App;
+export default App
